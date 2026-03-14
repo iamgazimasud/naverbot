27c960eb5f6056dd40e77f7f558468380ba9d1ef
Gazi Masud <mdmasud20555@gmail.com>
2026-03-15 02:10:28 +0900

    adding first variable

--- Index.js
+++ Index.js
@@ -1,3 +1,15 @@
 console.log("Hello World")
 
 alert("Welcome to Naver Bot")
+ var price = 300;
+ var age;
+
+var age;
+console.log(age);
+
+var price;
+console.log(price);
+
+var age = 35;
+age = 40;
+console.log(age);
